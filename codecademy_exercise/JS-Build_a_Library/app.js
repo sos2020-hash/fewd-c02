d5bc4e3768fa326225ec81f5bb75283823ab4970
Alex Chiu <qweiop525@gmail.com>
2021-08-03 11:04:16 +0800

    JS-class error

---
+++ codecademy_exercise/JS-Build_a_Library/app.js
@@ -0,0 +1,106 @@
+class Media {
+    constructor(title) {
+      this._title = title;
+      this._isCheckedOut = false;
+      this._ratings = [];
+    }
+
+    get title() {
+        return this._title;
+    }
+
+    get isCheckedOut() {
+        return this._isCheckedOut;
+    }
+
+    get ratings() {
+        return this._ratings;
+    }
+  
+    set isCheckedOut(isCheckedOut) {
+      this._isCheckedOut = isCheckedOut;
+    }
+
+    toggleCheckOutStatus() {
+       this._isCheckedOut = !this._isCheckedOut;
+    }
+
+    getAverageRating() {
+        const total = this._ratings.reduce((accumulator, currentValue) => {
+            return accumulator + currentValue
+        });
+        return total / this._ratings.length;
+    }
+
+    addRating(newRating) {
+        this._ratings.push(newRating);
+    }
+  };
+
+  class Book extends Media {
+      constructor(title, author, pages) {
+          super(title);
+          this._author = author;
+          this._pages = pages;
+      }
+
+      get author() {
+          return this._author;
+      }
+
+      get pages() {
+          return this._pages;
+      }
+  };
+
+  class Movie extends Media {
+      constructor(title, director, runTime) {
+          super(title);
+          this._director = director;
+          this._runTime = runTime;
+      }
+
+      get director() {
+          return this._director;
+      }
+
+      get reunTime() {
+          return this._runTime;
+      }
+  };
+
+  const historyOfEverything = new Book('A Short History of Nearly Everything', 'Bill Bryson', 544);
+  historyOfEverything.toggleCheckOutStatus();
+  console.log(historyOfEverything.isCheckedOut);
+  historyOfEverything.addRating(4);
+  historyOfEverything.addRating(5);
+  historyOfEverything.addRating(5);
+  console.log(historyOfEverything.getAverageRating());
+
+  const speed = new Movie('Speed', 'Jan de Bont', 116);
+  speed.toggleCheckOutStatus();
+  console.log(speed.isCheckedOut);
+  speed.addRating(1);
+  speed.addRating(1);
+  speed.addRating(5);
+  console.log(speed.getAverageRating());
+
+  class CD extends Media {
+      constructor (title , artist, songs) {
+          super(title);
+          this._artist = artist;
+          this._songs = [songs];
+      }
+
+      get artist() {
+          return this._artist;
+      }
+
+      get songs() {
+          return this._songs;
+      }
+
+      shuffle() {
+          
+      }
+  }
