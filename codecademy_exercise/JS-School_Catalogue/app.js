0ebb89cae36e84f03bb42df2d960f9c2508cbd7f
Alex Chiu <qweiop525@gmail.com>
2021-08-03 11:07:37 +0800

    error

---
+++ codecademy_exercise/JS-School_Catalogue/app.js
@@ -0,0 +1,67 @@
+class School {
+    contructor(name, level, numberOfStudents) {
+        this._name = name;
+        this._level = level;
+        this._numberOfStudents = numberOfStudents;
+    }
+    get name() {
+        return this._name;
+    }
+
+    get level() {
+        return this._level;
+    }
+
+    get numberOfStudents() {
+        return this._numberOfStudents;
+    }
+
+    set numberOfStudents(number) {
+        if (typeof number === 'number') {
+            return this._numberOfStudents = number;
+        } else {
+            console.log('Invalid input: numberOfStudents must be set to a Number.');
+        }
+    }
+
+    quickFacts() {
+        console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`);
+    }
+
+    static pickSubstituteTeacher(substituteTeachers) {
+        return substituteTeachers[Math.floor(substituteTeachers.length * Math.random())];
+    }
+};
+
+class PrimarySchool extends School {
+    constructor(name, numberOfStudents, pickupPolicy) {
+        super(name, 'primary', numberOfStudents);
+        this._pickupPolicy = pickupPolicy;
+    }
+
+    get pickupPolicy() {
+        return this._pickupPolicy;
+    }
+};
+
+const lorraineHansbury = new PrimarySchool('Lorraine Hansbury', 514, 'Students must be picked up by a parent, guardian, or a family member over the age of 13.');
+
+
+lorraineHansbury.quickFacts();
+
+  // class HighSchool extends School {
+  //   constructor(name, numberOfStudents, sportsTeams) {
+  //     super(name, 'high', numberOfStudents);
+  //     this._sportsTeams = sportsTeams;
+  //   }
+
+  //   get sportsTeams() {
+  //     return console.log(this._sportsTeams);
+  //   }
+  // };
+
+  // School.pickSubstituteTeacher(['Jamal Crawford', 'Lou Williams', 'J. R. Smith', 'James Harden', 'Jason Terry', 'Manu Ginobli']);
+
+  // const alSmith = new HighSchool('Al E. Smith', 415, ['Baseball', 'Basketball', 'Volleyball', 'Track and Field']);
+
+  // alSmith.sportsTeams;
